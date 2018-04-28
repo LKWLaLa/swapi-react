1240f010bc05bb49772fbdc40ee92b0c44636bcc
LKWLaLa <lkwlala@gmail.com>
2018-04-28 14:38:28 -0400

    add Home component, create components directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
+import Home from './Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header"></header>
+        <Home />
       </div>
     );
   }
