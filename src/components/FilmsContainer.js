a876de4abbd474a107b3639eb7e0d2970f5fed5f
LKWLaLa <lkwlala@gmail.com>
2018-04-28 21:04:39 -0400

    fetch data from api or display error msg

--- src/components/FilmsContainer.js
+++ src/components/FilmsContainer.js
@@ -1,9 +1,46 @@
 import React, {Component} from 'react'
+import StarWars from '../starWars.json'
 
 class FilmsContainer extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      characterId: parseInt(this.props.location.pathname.replace('/', ''), 0),
+      films: [],
+      errorMsg: ''
+    }
+  }
+
+  componentDidMount(){
+    let id = this.state.characterId
+    let url = StarWars.characters[id].url
+    fetch(url).then(res => res.json())
+    .then(res => {
+        let films = []
+        let total = res.films.length
+        res.films.forEach(
+          (filmUrl) => {
+            fetch(filmUrl).then(res => res.json())
+            .then(res => {
+              films.push(res)
+              if(films.length === total){
+                this.setState({films: films})
+              }
+            })
+          })
+      })
+    .catch(err => 
+      this.setState({errorMsg: "The greatest teacher, failure is.  The requested character could not be found."}))
+  }
+
   render(){
     return(
-      <div>FilmsContainer</div>
+      <div>
+        FilmsContainer
+        <div>{this.state.errorMsg}</div>
+      </div>
     )
   }
 } 
