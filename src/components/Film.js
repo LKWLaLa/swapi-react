4f1f29d2d4fb9a2dd0044abc80839bf28d4a0cbc
LKWLaLa <lkwlala@gmail.com>
2018-04-28 22:43:31 -0400

    add formatted date

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const Film = (props) => {
+  let {title, release_date, opening_crawl} = props.data
+
+  let formatted_date = () => {
+    var options = { weekday: 'long', month: 'long', day: 'numeric' }
+    let date  = new Date(release_date.replace(/-/g, '/'))
+    return `${date.toLocaleDateString("en-US",options)} ${date.getFullYear()}`
+  }
 
   return(
-    <div>
-      film
+    <div className="film">
+      <h2>{title}</h2>
+      <h3>{formatted_date()}</h3>
+      <p>{opening_crawl}</p>
     </div>
   )
 }
